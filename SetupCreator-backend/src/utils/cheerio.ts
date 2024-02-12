1d89a3bb577bd4f661ab5c67c16dd869d832275a
ninajbel <pawel200326598@wp.pl>
2024-02-12 03:26:51 +0100

    cheerio scraper types

--- SetupCreator-backend/src/utils/cheerio.ts
+++ SetupCreator-backend/src/utils/cheerio.ts
@@ -1,21 +1,30 @@
-import cheerio from "cheerio";
+import cheerio, { Cheerio } from "cheerio";
 
 export const scrapProductDetails = async (ceneoId: string) => {
-  const req = await fetch(`https://www.ceneo.pl/${ceneoId}`);
-  const html = await req.text();
+  const req: Response = await fetch(`https://www.ceneo.pl/${ceneoId}`);
+  const html: string = await req.text();
 
   const page = cheerio.load(html);
-  const priceMetaVal = page('meta[property="product:price:amount"]')?.attr(
-    "content"
+  const priceMetaVal: string | undefined = page(
+    'meta[property="product:price:amount"]'
+  )?.attr("content");
+  const imageMetaVal: string | undefined = page(
+    'meta[property="og:image"]'
+  )?.attr("content");
+
+  const price: number | null = priceMetaVal
+    ? parseInt(priceMetaVal) * 100
+    : null;
+  const image: string | null = imageMetaVal ? imageMetaVal : null;
+
+  const summaryShopWrapper: Cheerio<any> = page(
+    ".product-offer-summary__shop-logo a"
   );
-  const imageMetaVal = page('meta[property="og:image"]')?.attr("content");
 
-  const price = priceMetaVal ? parseInt(priceMetaVal) * 100 : null;
-  const image = imageMetaVal ? imageMetaVal : null;
-
-  const summaryShopWrapper = page(".product-offer-summary__shop-logo a");
-  const summaryShopUrl = summaryShopWrapper.attr("href");
-  const summaryShopImage = summaryShopWrapper.find("img").attr("src");
+  const summaryShopUrl: string | undefined = summaryShopWrapper.attr("href");
+  const summaryShopImage: string | undefined = summaryShopWrapper
+    .find("img")
+    .attr("src");
 
   return {
     price,
